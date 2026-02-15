75705a4d071b5bd8eec579694efa1c32154b4528
magicworldz <1940295+zhaojunlucky@users.noreply.github.com>
2026-02-15 17:26:11 +0800

    fix key import export issue

--- pkg/security/ecies_helper.go
+++ pkg/security/ecies_helper.go
@@ -2,6 +2,10 @@ package security
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"fmt"
+	"math/big"
+
 	"github.com/hotstar/ecies"
 )
 
@@ -11,27 +15,45 @@ type ECIESHelper struct {
 func (eciesHelper *ECIESHelper) EncryptWithPublic(key *ecdsa.PublicKey, data []byte) ([]byte, error) {
 	cipher := ecies.NewECIES()
 
-	pubKey := eciesHelper.importPublic(key)
+	pubKey, err := eciesHelper.importPublic(key)
+	if err != nil {
+		return nil, err
+	}
 	return cipher.Encrypt(pubKey, data)
 }
 
-func (aesHelper *ECIESHelper) importPublic(key *ecdsa.PublicKey) *ecies.PublicKey {
-	return &ecies.PublicKey{
-		Curve: key.Curve,
-		X:     key.X,
-		Y:     key.Y,
+func (eciesHelper *ECIESHelper) importPublic(key *ecdsa.PublicKey) (*ecies.PublicKey, error) {
+	pubBytes, err := key.Bytes()
+	if err != nil {
+		return nil, err
 	}
+	x, y := elliptic.Unmarshal(key.Curve, pubBytes)
+	if x == nil || y == nil {
+		return nil, fmt.Errorf("invalid public key")
+	}
+	return &ecies.PublicKey{Curve: key.Curve, X: x, Y: y}, nil
 }
 
-func (eciesHelper *ECIESHelper) importPrivate(key *ecdsa.PrivateKey) *ecies.PrivateKey {
-	return &ecies.PrivateKey{
-		PublicKey: eciesHelper.importPublic(&key.PublicKey),
-		D:         key.D,
+func (eciesHelper *ECIESHelper) importPrivate(key *ecdsa.PrivateKey) (*ecies.PrivateKey, error) {
+	pubKey, err := eciesHelper.importPublic(&key.PublicKey)
+	if err != nil {
+		return nil, err
 	}
+	privBytes, err := key.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	return &ecies.PrivateKey{
+		PublicKey: pubKey,
+		D:         new(big.Int).SetBytes(privBytes),
+	}, nil
 }
 
 func (eciesHelper *ECIESHelper) DecryptWithPrivate(key *ecdsa.PrivateKey, data []byte) ([]byte, error) {
 	cipher := ecies.NewECIES()
-	priKey := eciesHelper.importPrivate(key)
+	priKey, err := eciesHelper.importPrivate(key)
+	if err != nil {
+		return nil, err
+	}
 	return cipher.Decrypt(priKey, data)
 }
